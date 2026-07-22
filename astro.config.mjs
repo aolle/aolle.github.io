5f32c76138a2c905d97a09520e5ac06cd3f028ed
Àngel Ollé Blázquez <angel@olleb.com>
2026-07-22 13:49:45 +0200

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,17 @@ export default defineConfig({
       theme: 'github-light',
     },
   },
+  redirects: {
+    // OWASP WebGoat Hijack Session
+    '/OWASP-WebGoat-hijack-session': '/posts/2023-08-15-owasp-webgoat-hijack-session/',
+    '/OWASP-WebGoat-hijack-session/': '/posts/2023-08-15-owasp-webgoat-hijack-session/',
+
+    // OWASP WebGoat Spoof Auth Cookie
+    '/OWASP-WebGoat-spoof-auth-cookie': '/posts/2023-08-09-owasp-webgoat-spoof-auth-cookie/',
+    '/OWASP-WebGoat-spoof-auth-cookie/': '/posts/2023-08-09-owasp-webgoat-spoof-auth-cookie/',
+
+    // I2C Signal Reversing
+    '/I2C-signal-reversing': '/posts/2023-02-01-i2c-signal-reversing/',
+    '/I2C-signal-reversing/': '/posts/2023-02-01-i2c-signal-reversing/',
+  },
 });
